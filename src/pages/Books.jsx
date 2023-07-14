9a97dcc7c1c11f584e6b182531e50179affa9f1d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-14 00:29:37 -0400

    Create the Books component

---
+++ src/pages/Books.jsx
@@ -0,0 +1,11 @@
+import Layout from './Layout';
+
+const Books = () => (
+  <Layout>
+    <section className="page" id="books">
+      <h1>Books</h1>
+    </section>
+  </Layout>
+);
+
+export default Books;
