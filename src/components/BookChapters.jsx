680bc0f4e218673568151bfb1d77932bb7a081a6
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-14 13:49:50 -0400

    Refactor the Book chapter section to display full information of the chapters

--- src/components/BookChapters.jsx
+++ src/components/BookChapters.jsx
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 const BookChapters = ({ chapterInfo }) => (
   <div className="book-chapters-info">
     <div className="book-progress">{chapterInfo.completed}</div>
-    <div className="book-chapters">{chapterInfo.chapter}</div>
+    <div className="book-chapters">
+      <p className="book-current-chapter">CURRENT CHAPTER</p>
+      <p className="book-chapters-count">
+        Chapter
+        {' '}
+        {chapterInfo.chapter}
+      </p>
+      <button type="button" className="book-update-progress">UPDATE PROGRESS</button>
+    </div>
   </div>
 );
 
