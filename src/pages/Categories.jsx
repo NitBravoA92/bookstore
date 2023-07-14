9d80fdc3d90eeb02d4c79ffe80e9936e1ffd3d72
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-14 00:29:23 -0400

    Create the Categories component

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,12 @@
+import Layout from './Layout';
+
+const Categories = () => (
+  <Layout>
+    <section className="page" id="categories">
+      <h1>Categories</h1>
+      <h2>This page is not available.</h2>
+    </section>
+  </Layout>
+);
+
+export default Categories;
