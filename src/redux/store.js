822d5c83fbe26ad9d4701896bc0587fe09bdb3e4
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 14:43:17 -0400

    Import the categoriesSlice in the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookSlice from "./books/bookSlice";
+import categoriesSlice from "./categories/categoriesSlice";
 
 const store = configureStore({
   reducer: {
     books: bookSlice,
+    categories: categoriesSlice,
   },
 });
 
