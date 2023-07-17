a210c072e72e4d2533985b3bd13a41f07f50cd8f
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 14:43:43 -0400

    Import and use the categories state in the Categories page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,13 +1,21 @@
-import Layout from './Layout';
+import { useSelector, useDispatch } from "react-redux";
+import Layout from "./Layout";
 
-const Categories = () => (
-  <Layout>
-    <section className="page" id="categories">
-      <div className="container">
-        <h2>This page will be developed soon...</h2>
-      </div>
-    </section>
-  </Layout>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  dispatch({ type: "categories/getStatus" });
+
+  const categories = useSelector((state) => state.categories);
+
+  return (
+    <Layout>
+      <section className="page" id="categories">
+        <div className="container">
+          <h2>{categories}</h2>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
 export default Categories;
