9bcb6ef129cd98324ef002b793b1f27683641c06
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 13:12:52 -0400

    Create the bookSlice.js file to configure the books reducer with its actions

---
+++ src/redux/books/bookSlice.js
@@ -0,0 +1,20 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+const bookSlice = createSlice({ 
+  name: 'books',
+  initialState: [],
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      const findBook = state.find((book) => book.id === action.payload);
+      if(findBook) {
+        state.splice(state.indexOf(findBook), 1);
+      }
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+export default bookSlice.reducer;
