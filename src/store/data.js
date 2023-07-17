04f6a96c981b51eb3b36708b425fc31331814519
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-17 13:15:30 -0400

    Refactor the data.js module to export the BookElement function that generate a new Book object

--- src/store/data.js
+++ src/store/data.js
@@ -1,34 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const booksData = [
-  {
-    id: uuidv4(),
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    completed: '64%',
-    chapter: '17',
-  },
-  {
-    id: uuidv4(),
-    title: 'Dune',
-    author: 'Frank Herbert',
-    completed: '8%',
-    chapter: '3',
-  },
-  {
-    id: uuidv4(),
-    title: 'The Lord of the Rings',
-    author: 'J. R. R. Tolkien',
-    completed: '50%',
-    chapter: '10',
-  },
-  {
-    id: uuidv4(),
-    title: 'The Hobbit',
-    author: 'J. R. R. Tolkien',
-    completed: '80%',
-    chapter: '25',
-  },
-];
+const BookElement = (title, author) => ({
+  id: uuidv4(),
+  title,
+  author,
+  completed: '64%',
+  chapter: '17',
+});
 
-export default booksData;
+export default BookElement;
