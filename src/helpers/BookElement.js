7aade21e790a38e78368e02b2d5a585fbb2eea2d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-18 16:15:55 -0400

    Update the BookElement object to add category property

--- src/helpers/BookElement.js
+++ src/helpers/BookElement.js
@@ -1,12 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const BookElement = (title, author) => ({
-  id: uuidv4(),
+  item_id: uuidv4(),
   title,
   author,
-  category: "Fiction",
-  completed: '64%',
-  chapter: '17',
+  category: 'Fiction',
 });
 
 export default BookElement;
